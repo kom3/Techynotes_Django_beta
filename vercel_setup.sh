e40e97ee8f8e1545dc8dcc1bed66a57faafbc21f
manjunath.o <manjunath.o@justdial.com>
2022-06-02 11:19:48 +0530

    added vercel config file

--- vercel_setup.sh
+++ vercel_setup.sh
@@ -1,9 +1,12 @@
-apt-get install python3-distutils
-
+# apt-get install python3-distutils
+# apt-get install gcc libpq-dev -y
+# apt-get install python-dev  python-pip -y
+# apt-get install python3-dev python3-pip python3-venv python3-wheel -y
 # Update pip
+# python3.9 -m pip install --upgrade pip
+apt-get install git
+pip install setuptools
 pip install wheel
-python3.9 -m pip install --upgrade pip
-
 pip install -r requirements.txt
 
 python3.9 manage.py collectstatic
